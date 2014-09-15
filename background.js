89e84c4b5cab460094d7776df13fd8819b8cc545
Nagappan Nachiappan <n4nagappan@gmail.com>
2014-09-16 00:30:48 +0800

    Adding order form

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // will be called when the app is started
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('vcs.html', {
+  chrome.app.window.create('order.html', {
     'bounds': {
       'width': 800,
       'height': 800
