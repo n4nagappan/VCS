40080502ed07090b9be8db72eaf1c020b1bc83f3
Nagappan Nachiappan <n4nagappan@gmail.com>
2014-09-17 02:59:47 +0800

    Add date picker for delivery date

--- js/app.js
+++ js/app.js
@@ -6,15 +6,26 @@
         function($scope) {
 
             $scope.order = {};
+
+            $('.date').datepicker({
+                autoclose: true,
+                format: "dd/mm/yy"
+            }).on("changeDate", function(e) {
+                console.log(e);
+                console.log($(this).val());
+                $(this).trigger('input');
+            });
+
             $scope.submit = function() {
                 console.log($scope.order);
+                console.log($scope.order.deliveryDate);
             };
 
             $scope.recalculate = function() {
                 $scope.order.total = $scope.order.price + $scope.order.deliveryCharge;
                 $scope.order.balance = $scope.order.total - $scope.order.advance;
             };
-            
+
             $scope.clearValues = function() {
                 $scope.order = {};
             };
