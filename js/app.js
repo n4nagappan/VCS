21378b57e32807238a2f4e1210f4a2d5eeaa3e7a
Nagappan Nachiappan <n4nagappan@gmail.com>
2014-09-20 15:06:45 +0800

    1. Added angular-csp.css to handle ng-show/ng-hide 2. Moved the OrderController to orderPage directive's link 3. Added navigation show/hide based on the menu item selected

--- js/app.js
+++ js/app.js
@@ -2,40 +2,51 @@
     // Currently ng-app declared in order.html 
     // move it to vcs.html 
     var app = angular.module('vcs', []);
-    app.controller('orderController', ['$scope',
-        function($scope) {
+    app.controller('navController', function() {
+        this.currentMenu = "Home";
 
-            $scope.order = {};
+        this.setMenu = function(menuItem) {
+            this.currentMenu = menuItem;
+        };
 
-            $('.date').datepicker({
-                autoclose: true,
-                format: "dd/mm/yy"
-            }).on("changeDate", function(e) {
-                console.log(e);
-                console.log($(this).val());
-                $(this).trigger('input');
-            });
-
-            $scope.submit = function() {
-                console.log($scope.order);
-                console.log($scope.order.deliveryDate);
-            };
-
-            $scope.recalculate = function() {
-                $scope.order.total = $scope.order.price + $scope.order.deliveryCharge;
-                $scope.order.balance = $scope.order.total - $scope.order.advance;
-            };
-
-            $scope.clearValues = function() {
-                $scope.order = {};
-            };
-        }
-    ]);
+        this.isCurrentMenu = function(menuItem) {
+            var b = (this.currentMenu === menuItem);
+            console.log(b);
+            return b;
+        };
+    });
 
     app.directive('orderPage', function() {
         return {
             restrict: "E",
-            templateUrl: "order.html"
+            templateUrl: "views/order.html",
+            scope: {},
+            link: function($scope, $element , attr ) {
+                $scope.order = {};
+
+                $('.date').datepicker({
+                    autoclose: true,
+                    format: "dd/mm/yy"
+                }).on("changeDate", function(e) {
+                    console.log(e);
+                    console.log($(this).val());
+                    $(this).trigger('input');
+                });
+
+                $scope.submit = function() {
+                    console.log($scope.order);
+                    console.log($scope.order.deliveryDate);
+                };
+
+                $scope.recalculate = function() {
+                    $scope.order.total = $scope.order.price + $scope.order.deliveryCharge;
+                    $scope.order.balance = $scope.order.total - $scope.order.advance;
+                };
+
+                $scope.clearValues = function() {
+                    $scope.order = {};
+                };
+            }
         };
     });
 })();
